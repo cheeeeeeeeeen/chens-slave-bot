f8f9168abfb77ebb202f906f516ed04195b83b0a
Cheeeeeeeeeen <blancfaye7@gmail.com>
2020-03-09 01:09:29 +0800

    Allow features to accept assembler object

--- app/bot/features/base.rb
+++ app/bot/features/base.rb
@@ -1,10 +1,11 @@
 module Bot
   module Features
     class Base
-      attr_reader :bot
+      attr_reader :bot, :assembler
 
-      def initialize(bot_obj)
+      def initialize(bot_obj, assembler_obj)
         @bot = bot_obj
+        @assembler = assembler_obj
       end
 
       def insert
